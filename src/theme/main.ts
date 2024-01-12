92d89fd92df52c5f1c77791f5ad0edd20d57551c
Deimonn <deimonn@outlook.com>
2024-01-12 00:35:49 -0300

    Make `mainTheme` into a function so it returns a new instance every call

--- src/theme/main.ts
+++ src/theme/main.ts
@@ -12,42 +12,44 @@
 import { Theme } from "../theme"
 
 /** Main theme definition. */
-export const mainTheme: Theme = {
-    name: "Oro Theme",
-    filename: "mainTheme.json",
+export function mainTheme(): Theme {
+    return {
+        name: "Oro Theme",
+        filename: "mainTheme.json",
 
-    keyword: "#e0cd78",
-    operator: "#e0cd78",
+        keyword: "#e0cd78",
+        operator: "#e0cd78",
 
-    userType: "#63dbc1",
-    builtinType: "#70c2e7",
-    interfaceType: "#e4c693",
+        userType: "#63dbc1",
+        builtinType: "#70c2e7",
+        interfaceType: "#e4c693",
 
-    function: "#dddddd",
-    staticFunction: "#70c2e7",
+        function: "#dddddd",
+        staticFunction: "#70c2e7",
 
-    constant: "#77d4d4",
-    builtinConstant: "#77d4d4",
+        constant: "#77d4d4",
+        builtinConstant: "#77d4d4",
 
-    enumeration: "#8FDB8F",
-    namespace: "#d5ccba",
-    macro: "#a0a0a0",
-    variable: "#bcd3d6",
-    number: "#cec7a8",
-    punctuation: "#a0a0a0",
-    comment: "#787878",
+        enumeration: "#8FDB8F",
+        namespace: "#d5ccba",
+        macro: "#a0a0a0",
+        variable: "#bcd3d6",
+        number: "#cec7a8",
+        punctuation: "#a0a0a0",
+        comment: "#787878",
 
-    string: "#68e09e",
-    escape: "#a8dbbf",
+        string: "#68e09e",
+        escape: "#a8dbbf",
 
-    error: "#e04444",
-    warning: "#e0a243",
-    link: "#4eb3e2",
+        error: "#e04444",
+        warning: "#e0a243",
+        link: "#4eb3e2",
 
-    ui: {
-        accent: "#70c2e7",
-        border: "#2e505f",
-        button: "#3a6375",
-        buttonHover: "#4f88a0"
+        ui: {
+            accent: "#70c2e7",
+            border: "#2e505f",
+            button: "#3a6375",
+            buttonHover: "#4f88a0"
+        }
     }
 }
