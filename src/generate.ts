8dbbcfc2346f2adc4daf2230534c05d958d5d24b
Deimonn <deimonn@outlook.com>
2024-01-14 17:53:10 -0300

    Extend generator to allow for better UI customization

--- src/generate.ts
+++ src/generate.ts
@@ -141,7 +141,7 @@ function generateTheme(theme: OroTheme): VSCodeTheme {
             {
                 scope: "invalid.illegal",
                 settings: {
-                    foreground: built.error,
+                    foreground: built.error.hexa(),
                     fontStyle: ""
                 }
             },
@@ -313,52 +313,359 @@ function generateTheme(theme: OroTheme): VSCodeTheme {
             }
         ],
         colors: {
-            "editor.background": "#161616",
-            "editorError.background": built.error + "7f",
-            "editorError.border": built.error,
-            "editorError.foreground": built.error,
-            "editorWarning.background": built.warning + "7f",
-            "editorWarning.border": built.warning,
-            "editorWarning.foreground": built.warning,
-            "list.errorForeground": built.error,
-            "list.warningForeground": built.warning,
-            "activityBar.activeBackground": "#1C1C1C",
-            "activityBar.activeBorder": built.ui.accent,
-            "activityBar.activeFocusBorder": built.ui.accent,
-            "activityBar.background": "#161616",
-            "activityBar.dropBorder": built.ui.accent,
-            "activityBar.inactiveForeground": "#b9b9b9",
-            "activityBarBadge.background": "#DDDDDD",
-            "activityBarBadge.foreground": "#161616",
-            "button.background": built.ui.button,
-            "button.foreground": "#DDDDDD",
-            "button.hoverBackground": built.ui.buttonHover,
-            "checkbox.background": "#303030",
-            "dropdown.background": "#202020",
-            "editor.foreground": "#DDDDDD",
-            "editorGroupHeader.tabsBackground": "#1C1C1C",
-            "editorLineNumber.activeForeground": "#b9b9b9",
-            "editorLineNumber.foreground": "#5f5f5f",
-            "editorLink.activeForeground": built.link,
-            "focusBorder": built.ui.accent,
-            "input.background": "#202020",
-            "input.border": built.ui.border,
-            "peekView.border": built.ui.accent,
-            "peekViewEditor.background": "#202020",
-            "peekViewEditor.matchHighlightBackground": built.ui.accent + "62",
-            "peekViewEditorGutter.background": "#202020",
-            "peekViewResult.matchHighlightBackground": built.ui.accent + "62",
-            "sideBar.background": "#1C1C1C",
-            "sideBarSectionHeader.background": "#1C1C1C",
-            "statusBar.background": built.ui.button,
-            "tab.activeBackground": "#161616",
-            "tab.inactiveBackground": "#1C1C1C",
-            "textLink.activeForeground": built.link,
-            "textLink.foreground": built.ui.accent,
-            "textPreformat.foreground": "#ffffff",
-            "textSeparator.foreground": "#161616",
-            "titleBar.activeBackground": "#1C1C1C",
-            "titleBar.inactiveBackground": "#1C1C1C",
+            /*─ Misc ─────────────────────────────────────────────────────────*/
+
+            // Foregrounds.
+            "foreground": built.foreground.hex(),
+            "errorForeground": built.error.hex(),
+
+            // Badge.
+            "badge.background": built.foreground.hex(),
+            "badge.foreground": built.background.hex(),
+
+            // Modified setting.
+            "settings.modifiedItemIndicator": built.modified.hex(),
+
+            // Diff.
+            "diffEditor.insertedLineBackground": built.added.fade(.90).hexa(),
+            "diffEditor.insertedTextBackground": built.added.fade(.80).hexa(),
+            "diffEditor.removedLineBackground": built.deleted.fade(.80).hexa(),
+            "diffEditor.removedTextBackground": built.deleted.fade(.80).hexa(),
+
+            // Drop background.
+            "list.dropBackground": built.guide.fade(.75).hexa(),
+            "terminal.dropBackground": built.guide.fade(.75).hexa(),
+            "sideBar.dropBackground": built.guide.fade(.75).hexa(),
+            "editorGroup.dropBackground": built.guide.fade(.75).hexa(),
+            "panelSection.dropBackground": built.guide.fade(.75).hexa(),
+
+            // Progress bar.
+            "progressBar.background": built.accent.hex(),
+
+            /*─ Editor ───────────────────────────────────────────────────────*/
+
+            // General.
+            "editor.background": built.background.hex(),
+            "editor.foreground": built.foreground.hex(),
+            "editorPane.background": built.background.darken(.20).hex(),
+            "editorCursor.background": built.background.hex(),
+            "editorCursor.foreground": built.foreground.hex(),
+            "editorLink.activeForeground": built.linkHover.hex(),
+            "editorOverviewRuler.border": built.background.hex(),
+
+            // Guides.
+            "editor.foldBackground": built.guide.darken(.50).hex(),
+
+            "editor.lineHighlightBackground": built.background.hex(),
+            "editor.lineHighlightBorder": built.guide.fade(.50).hexa(),
+
+            "editorLineNumber.activeForeground": built.guideFocus.hex(),
+            "editorLineNumber.foreground": built.guide.hex(),
+
+            "editorRuler.foreground": built.guide.hex(),
+            "editorWhitespace.foreground": built.guide.hex(),
+
+            // Bracket matching and indentation.
+            "editorBracketMatch.background": "#00000000",
+            "editorBracketMatch.border": built.punctuation.color.hex(),
+
+            "editorOverviewRuler.bracketMatchForeground":
+                built.punctuation.color.hex(),
+
+            "editorIndentGuide.activeBackground":
+                built.guideFocus.darken(.50).hex(),
+            "editorIndentGuide.background":
+                built.guide.darken(.50).hex(),
+
+            // Tabs.
+            "tab.activeBackground": built.background.hex(),
+            "tab.inactiveBackground": built.backgroundSecondary.hex(),
+            "tab.border": "#00000000",
+            "tab.activeBorder": "#00000000",
+            "tab.activeBorderTop": built.accent.hex(),
+            "tab.hoverBackground": built.backgroundSecondary.lighten(.25).hex(),
+            "tab.lastPinnedBorder": built.guide.hex(),
+
+            "editorGroupHeader.noTabsBackground":
+                built.backgroundSecondary.hex(),
+            "editorGroupHeader.tabsBackground":
+                built.backgroundSecondary.hex(),
+
+            "editorGroup.border": built.guide.hex(),
+
+            // Peek view.
+            "peekView.border": built.accent.hex(),
+            "peekViewEditor.background": built.backgroundPeek.hex(),
+            "peekViewEditorGutter.background": built.backgroundPeek.hex(),
+
+            "peekViewEditor.matchHighlightBackground":
+                built.accent.fade(.62).hexa(),
+            "peekViewResult.matchHighlightBackground":
+                built.accent.fade(.62).hexa(),
+
+            "peekViewResult.background": built.backgroundSecondary.hex(),
+            "peekViewTitle.background": built.accent.darken(.75).hex(),
+
+            // Various widgets.
+            "editorHoverWidget.background": built.widget.hex(),
+            "editorHoverWidget.border": built.widgetBorder.hex(),
+
+            "editorSuggestWidget.background": built.widget.hex(),
+            "editorSuggestWidget.border": built.widgetBorder.hex(),
+            "editorSuggestWidget.highlightForeground": built.accent.hex(),
+
+            "editorWidget.background": "#302843",
+            "editorWidget.resizeBorder": "#42375d",
+
+            // Selection.
+            "editor.selectionBackground": built.accent.fade(.80).hexa(),
+            "editor.selectionHighlightBorder": built.accent.fade(.60).hexa(),
+
+            "minimap.selectionHighlight":
+                built.accent.fade(.60).hexa(),
+            "editorOverviewRuler.selectionHighlightForeground":
+                built.accent.fade(.60).hexa(),
+
+            // Search.
+            "editor.findMatchBorder": built.matchBorder.hex(),
+            "editor.findMatchHighlightBackground": built.match.fade(.50).hexa(),
+            "editorOverviewRuler.findMatchForeground": built.match.hex(),
+            "minimap.findMatchHighlight": built.match.hex(),
+
+            /*─ Interface ────────────────────────────────────────────────────*/
+
+            // Activity bar.
+            "activityBar.activeBackground": built.backgroundSecondary.hex(),
+            "activityBar.activeBorder": built.accent.hex(),
+            "activityBar.activeFocusBorder": built.accent.hex(),
+
+            "activityBar.background": built.background.hex(),
+            "activityBar.dropBorder": built.accent.hex(),
+            "activityBar.inactiveForeground": built.foregroundInactive.hex(),
+
+            "activityBarBadge.background": built.foreground.hex(),
+            "activityBarBadge.foreground": built.background.hex(),
+
+            // Side bar.
+            "sideBar.background": built.backgroundSecondary.hex(),
+            "sideBarSectionHeader.background": built.backgroundSecondary.hex(),
+
+            // Status bar.
+            "statusBar.foreground": built.statusBarForeground.hex(),
+            "statusBar.background": built.statusBar.hex(),
+            "statusBar.debuggingBackground": built.statusBarDebugging.hex(),
+            "statusBar.noFolderBackground": built.statusBarNoFolder.hex(),
+
+            "statusBarItem.remoteBackground": built.foreground.hex(),
+            "statusBarItem.remoteForeground": built.background.hex(),
+
+            "statusBarItem.remoteHoverBackground":
+                built.foreground.darken(.50).hex(),
+            "statusBarItem.remoteHoverForeground":
+                built.foreground.hex(),
+
+            "statusBar.focusBorder": built.statusBarForeground.hex(),
+
+            // Title bar.
+            "titleBar.activeBackground": built.backgroundSecondary.hex(),
+            "titleBar.inactiveBackground": built.backgroundSecondary.hex(),
+
+            // Scroll bar.
+            "scrollbarSlider.activeBackground": built.scrollBarActive.hexa(),
+            "scrollbarSlider.background": built.scrollBar.hexa(),
+            "scrollbarSlider.hoverBackground": built.scrollBar.hexa(),
+
+            // Panels.
+            "panel.dropBorder": built.accent.hex(),
+            "panelTitle.activeBorder": built.accent.hex(),
+            "terminal.selectionBackground": built.accent.fade(.80).hexa(),
+            "terminalCursor.background": built.background.hex(),
+            "terminalCursor.foreground": built.foreground.hex(),
+
+            // Notification.
+            "notifications.background": built.widget.hex(),
+            "notifications.border": built.widgetBorder.hex(),
+
+            // Lists.
+            "list.activeSelectionBackground": built.accent.fade(.75).hexa(),
+            "list.focusBackground": built.accent.fade(.75).hexa(),
+            "list.hoverBackground": built.accent.fade(.90).hexa(),
+            "list.highlightForeground": built.accent.hex(),
+            "list.inactiveSelectionBackground": built.widget.hex(),
+
+            // Menus.
+            "menu.background": built.widget.hex(),
+            "menu.foreground": built.foreground.hex(),
+            "menu.selectionBackground": built.accent.hex(),
+            "menu.selectionForeground": built.background.hex(),
+            "menu.separatorBackground": built.widgetBorder.hex(),
+
+            // All borders.
+            "focusBorder": built.accent.hex(),
+
+            /*─ Controls ─────────────────────────────────────────────────────*/
+
+            // Button.
+            "button.background": built.button.hex(),
+            "button.foreground": built.foreground.hex(),
+            "button.hoverBackground": built.buttonHover.hex(),
+
+            // Checkbox.
+            "checkbox.background": built.checkbox.hex(),
+
+            // Input.
+            "input.background": built.input.hex(),
+            "input.border": built.inputBorder.hex(),
+
+            // Dropdown.
+            "dropdown.background": built.input.hex(),
+            "dropdown.border": built.inputBorder.hex(),
+
+            /*─ Text ─────────────────────────────────────────────────────────*/
+
+            // Link.
+            "textLink.activeForeground": built.linkHover.hex(),
+            "textLink.foreground": built.link.hex(),
+
+            // Preformat.
+            "textPreformat.foreground": built.preformat.hex(),
+
+            // Separator.
+            "textSeparator.foreground": built.background.hex(),
+
+            // Selection.
+            "selection.background": built.accent.fade(.80).hexa(),
+
+            /*─ Resources ────────────────────────────────────────────────────*/
+
+            // Git decorations.
+            "gitDecoration.addedResourceForeground":
+                built.added.hex(),
+            "gitDecoration.deletedResourceForeground":
+                built.deleted.hex(),
+            "gitDecoration.stageDeletedResourceForeground":
+                built.deleted.hex(),
+            "gitDecoration.ignoredResourceForeground":
+                built.ignored.hex(),
+            "gitDecoration.modifiedResourceForeground":
+                built.modified.hex(),
+            "gitDecoration.stageModifiedResourceForeground":
+                built.modified.hex(),
+            "gitDecoration.untrackedResourceForeground":
+                built.untracked.hex(),
+
+            // Minimap.
+            "minimapGutter.addedBackground": built.added.hex(),
+            "minimapGutter.deletedBackground": built.deleted.hex(),
+            "minimapGutter.modifiedBackground": built.modified.hex(),
+
+            // Gutter.
+            "editorGutter.addedBackground": built.added.hex(),
+            "editorGutter.modifiedBackground": built.modified.hex(),
+            "editorGutter.deletedBackground": built.deleted.hex(),
+
+            // Overview ruler.
+            "editorOverviewRuler.addedForeground":
+                built.added.fade(.50).hexa(),
+            "editorOverviewRuler.modifiedForeground":
+                built.modified.fade(.50).hexa(),
+            "editorOverviewRuler.deletedForeground":
+                built.deleted.fade(.50).hexa(),
+
+            /*─ Diagnostics ──────────────────────────────────────────────────*/
+
+            // In editor.
+            "editorError.background": built.error.fade(.50).hexa(),
+            "editorError.foreground": built.error.hex(),
+            "editorError.border": built.error.hex(),
+
+            "editorWarning.background": built.warning.fade(.50).hexa(),
+            "editorWarning.foreground": built.warning.hex(),
+            "editorWarning.border": built.warning.hex(),
+
+            "editorInfo.background": built.info.fade(.75).hexa(),
+            "editorInfo.foreground": built.info.hex(),
+            "editorInfo.border": built.info.hex(),
+
+            // In list.
+            "list.errorForeground": built.error.hex(),
+            "list.warningForeground": built.warning.hex(),
+
+            // Problems.
+            "problemsErrorIcon.foreground": built.error.hex(),
+            "problemsWarningIcon.foreground": built.warning.hex(),
+            "problemsInfoIcon.foreground": built.info.hex(),
+
+            // Problem navigation.
+            "editorMarkerNavigation.background": built.widget.hex(),
+            "editorMarkerNavigationError.background": built.error.hex(),
+            "editorMarkerNavigationInfo.background": built.info.hex(),
+            "editorMarkerNavigationWarning.background": built.warning.hex(),
+
+            // Notifications.
+            "notificationsErrorIcon.foreground": built.error.hex(),
+            "notificationsWarningIcon.foreground": built.warning.hex(),
+            "notificationsInfoIcon.foreground": built.info.hex(),
+
+            // Minimap.
+            "minimap.background": built.background.hex(),
+            "minimap.errorHighlight": built.error.hex(),
+            "minimap.warningHighlight": built.warning.hex(),
+
+            // Overview ruler.
+            "editorOverviewRuler.errorForeground": built.error.hex(),
+            "editorOverviewRuler.warningForeground": built.warning.hex(),
+            "editorOverviewRuler.infoForeground": built.info.hex(),
+
+            // Input validation in settings.
+            "inputValidation.errorBackground":
+                built.error.darken(.75).hex(),
+            "inputValidation.errorBorder":
+                built.error.hex(),
+            "inputValidation.errorForeground":
+                built.error.lighten(.25).hex(),
+
+            "inputValidation.warningBackground":
+                built.warning.darken(.75).hex(),
+            "inputValidation.warningBorder":
+                built.warning.hex(),
+            "inputValidation.warningForeground":
+                built.warning.lighten(.25).hex(),
+
+            "inputValidation.infoBackground": built.info.darken(.75).hex(),
+            "inputValidation.infoBorder": built.info.hex(),
+            "inputValidation.infoForeground": built.info.lighten(.25).hex(),
+
+            /*─ ErrorLens ────────────────────────────────────────────────────*/
+            /* Special interaction with the ErrorLens extension.              */
+
+            // Error colors.
+            "errorLens.errorForeground": built.error.hex(),
+            "errorLens.errorBackground": built.error.fade(.90).hexa(),
+            "errorLens.errorMessageBackground": built.error.fade(.90).hexa(),
+            "errorLens.errorRangeBackground": "#00000000",
+
+            // Warning colors.
+            "errorLens.warningForeground":
+                built.warning.hex(),
+            "errorLens.warningBackground":
+                built.warning.fade(.90).hexa(),
+            "errorLens.warningMessageBackground":
+                built.warning.fade(.90).hexa(),
+            "errorLens.warningRangeBackground":
+                "#00000000",
+
+            // Info colors.
+            "errorLens.infoForeground": built.info.hex(),
+            "errorLens.infoBackground": built.info.fade(.90).hexa(),
+            "errorLens.infoMessageBackground": built.info.fade(.90).hexa(),
+            "errorLens.infoRangeBackground": "#00000000",
+
+            // Hint colors.
+            "errorLens.hintForeground": built.ignored.hex(),
+            "errorLens.hintBackground": built.ignored.fade(.90).hexa(),
+            "errorLens.hintMessageBackground": built.ignored.fade(.90).hexa(),
+            "errorLens.hintRangeBackground": "#00000000",
         }
     }
 }
