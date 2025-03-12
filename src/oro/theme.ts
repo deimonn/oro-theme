d1573de93d5fbf514bf537e245ffd1e635c48c3d
deimonn <deimonn@outlook.com>
2025-03-12 11:34:57 -0300

    Simplify theme type definitions

--- src/oro/theme.ts
+++ src/oro/theme.ts
@@ -13,228 +13,154 @@
 import { OroColor, BuiltColor, buildColor } from "./color";
 import { OroStyle, BuiltStyle, buildStyle } from "./style";
 
+/** Generic theme type. */
+type BaseTheme<S, C> = {
+    /** Style for keywords. */
+    keyword: S
+    /** Style for operators. */
+    operator: S
+
+    /** Style for user-defined types, such as classes. */
+    userType: S
+    /** Style for built-in types. */
+    builtinType: S
+    /** Style for interface or interface-like types. */
+    interfaceType: S
+
+    /** Style for functions. */
+    function: S
+    /** Style for static functions. */
+    staticFunction: S
+
+    /** Style for constants and enumeration members. */
+    constant: S
+    /** Style for built-in constants, such as `true`. */
+    builtinConstant: S
+
+    /** Style for enumerations. */
+    enumeration: S
+    /** Style for namespaces. */
+    namespace: S
+    /** Style for macros. */
+    macro: S
+    /** Style for variables. */
+    variable: S
+    /** Style for numbers. */
+    number: S
+    /** Style for punctuation. */
+    punctuation: S
+    /** Style for comments. */
+    comment: S
+
+    /** Style for strings. */
+    string: S
+    /** Style for escape sequences. */
+    escape: S
+
+    /** Color for errors and illegal code. */
+    error: C
+    /** Color for warnings. */
+    warning: C
+    /** Color for info. */
+    info: C
+
+    /** Color for added resources. */
+    added: C
+    /** Color for deleted resources. */
+    deleted: C
+    /** Color for ignored resources. */
+    ignored: C
+    /** Color for modified resources and settings. */
+    modified: C
+    /** Color for untracked resources. */
+    untracked: C
+
+    /**
+     * Color for "running" items in the interface; currently just used for running processes on a
+     * forwarded port.
+     */
+    running: C
+
+    /** Main background color; e.g. the editor's. */
+    background: C
+    /** Secondary background color; e.g. the sidebar's. */
+    backgroundSecondary: C
+    /** Peek background color. */
+    backgroundPeek: C
+
+    /** Default foreground color. */
+    foreground: C
+    /** Inactive foreground color. */
+    foregroundInactive: C
+
+    /** Preformatted text color. */
+    preformat: C
+    /** Color for accenting, focus and highlighting. */
+    accent: C
+
+    /** Guide color; e.g. indentation, line numbers. */
+    guide: C
+    /** Focused guide color. */
+    guideFocus: C
+
+    /** Color for search matches. */
+    match: C
+    /** Color for the border of the currently focused search match. */
+    matchFocus: C
+
+    /** Link color. */
+    link: C
+    /** Hovered link color. */
+    linkHover: C
+
+    /** Button color. */
+    button: C
+    /** Hovered button color. */
+    buttonHover: C
+
+    /** Input and dropdown color. */
+    input: C
+    /** Input border color. */
+    inputBorder: C
+
+    /** Widget color; e.g. background of hover, notifications. */
+    widget: C
+    /** Widget border color. */
+    widgetBorder: C
+
+    /** Checkbox color. */
+    checkbox: C
+
+    /** Scroll bar color. */
+    scrollBar: C
+    /** Scroll bar color when clicked. */
+    scrollBarActive: C
+
+    /** Status bar color. */
+    statusBar: C
+    /** Status bar foreground color. */
+    statusBarForeground: C
+    /** Status bar color during debugging. */
+    statusBarDebugging: C
+    /** Status bar color when no folder is open. */
+    statusBarNoFolder: C
+
+    /** Badge color. */
+    badge: C
+    /** Badge foreground color. */
+    badgeForeground: C
+};
+
 /** Oro theme definition object. */
 export type OroTheme = {
     /** The name to give to the theme. */
     name: string
     /** The name to give to the theme file. */
     filename: string
-
-    /** Style for keywords. */
-    keyword: OroStyle
-    /** Style for operators. */
-    operator: OroStyle
-
-    /** Style for user-defined types, such as classes. */
-    userType: OroStyle
-    /** Style for built-in types. */
-    builtinType: OroStyle
-    /** Style for interface or interface-like types. */
-    interfaceType: OroStyle
-
-    /** Style for functions. */
-    function: OroStyle
-    /** Style for static functions. */
-    staticFunction: OroStyle
-
-    /** Style for constants and enumeration members. */
-    constant: OroStyle
-    /** Style for built-in constants, such as `true`. */
-    builtinConstant: OroStyle
-
-    /** Style for enumerations. */
-    enumeration: OroStyle
-    /** Style for namespaces. */
-    namespace: OroStyle
-    /** Style for macros. */
-    macro: OroStyle
-    /** Style for variables. */
-    variable: OroStyle
-    /** Style for numbers. */
-    number: OroStyle
-    /** Style for punctuation. */
-    punctuation: OroStyle
-    /** Style for comments. */
-    comment: OroStyle
-
-    /** Style for strings. */
-    string: OroStyle
-    /** Style for escape sequences. */
-    escape: OroStyle
-
-    /** Color for errors and illegal code. */
-    error: OroColor
-    /** Color for warnings. */
-    warning: OroColor
-    /** Color for info. */
-    info: OroColor
-
-    /** Color for added resources. */
-    added: OroColor
-    /** Color for deleted resources. */
-    deleted: OroColor
-    /** Color for ignored resources. */
-    ignored: OroColor
-    /** Color for modified resources and settings. */
-    modified: OroColor
-    /** Color for untracked resources. */
-    untracked: OroColor
-
-    /**
-     * Color for "running" items in the interface; currently just used for running processes on a
-     * forwarded port.
-     */
-    running: OroColor
-
-    /** Main background color; e.g. the editor's. */
-    background: OroColor
-    /** Secondary background color; e.g. the sidebar's. */
-    backgroundSecondary: OroColor
-    /** Peek background color. */
-    backgroundPeek: OroColor
-
-    /** Default foreground color. */
-    foreground: OroColor
-    /** Inactive foreground color. */
-    foregroundInactive: OroColor
-
-    /** Preformatted text color. */
-    preformat: OroColor
-    /** Color for accenting, focus and highlighting. */
-    accent: OroColor
-
-    /** Guide color; e.g. indentation, line numbers. */
-    guide: OroColor
-    /** Focused guide color. */
-    guideFocus: OroColor
-
-    /** Color for search matches. */
-    match: OroColor
-    /** Color for the border of the currently focused search match. */
-    matchFocus: OroColor
-
-    /** Link color. */
-    link: OroColor
-    /** Hovered link color. */
-    linkHover: OroColor
-
-    /** Button color. */
-    button: OroColor
-    /** Hovered button color. */
-    buttonHover: OroColor
-
-    /** Input and dropdown color. */
-    input: OroColor
-    /** Input border color. */
-    inputBorder: OroColor
-
-    /** Widget color; e.g. background of hover, notifications. */
-    widget: OroColor
-    /** Widget border color. */
-    widgetBorder: OroColor
-
-    /** Checkbox color. */
-    checkbox: OroColor
-
-    /** Scroll bar color. */
-    scrollBar: OroColor
-    /** Scroll bar color when clicked. */
-    scrollBarActive: OroColor
-
-    /** Status bar color. */
-    statusBar: OroColor
-    /** Status bar foreground color. */
-    statusBarForeground: OroColor
-    /** Status bar color during debugging. */
-    statusBarDebugging: OroColor
-    /** Status bar color when no folder is open. */
-    statusBarNoFolder: OroColor
-
-    /** Badge color. */
-    badge: OroColor
-    /** Badge foreground color. */
-    badgeForeground: OroColor
-};
+} & BaseTheme<OroStyle, OroColor>;
 
 /** Built theme styles and colors. */
-export type BuiltTheme = {
-    keyword: BuiltStyle
-    operator: BuiltStyle
-
-    userType: BuiltStyle
-    builtinType: BuiltStyle
-    interfaceType: BuiltStyle
-
-    function: BuiltStyle
-    staticFunction: BuiltStyle
-
-    constant: BuiltStyle
-    builtinConstant: BuiltStyle
-
-    enumeration: BuiltStyle
-    namespace: BuiltStyle
-    macro: BuiltStyle
-    variable: BuiltStyle
-    number: BuiltStyle
-    punctuation: BuiltStyle
-    comment: BuiltStyle
-
-    string: BuiltStyle
-    escape: BuiltStyle
-
-    error: BuiltColor
-    warning: BuiltColor
-    info: BuiltColor
-
-    added: BuiltColor
-    deleted: BuiltColor
-    ignored: BuiltColor
-    modified: BuiltColor
-    untracked: BuiltColor
-
-    running: BuiltColor
-
-    background: BuiltColor
-    backgroundSecondary: BuiltColor
-    backgroundPeek: BuiltColor
-
-    foreground: BuiltColor
-    foregroundInactive: BuiltColor
-
-    preformat: BuiltColor
-    accent: BuiltColor
-
-    guide: BuiltColor
-    guideFocus: BuiltColor
-
-    match: BuiltColor
-    matchFocus: BuiltColor
-
-    link: BuiltColor
-    linkHover: BuiltColor
-
-    button: BuiltColor
-    buttonHover: BuiltColor
-
-    input: BuiltColor
-    inputBorder: BuiltColor
-
-    widget: BuiltColor
-    widgetBorder: BuiltColor
-
-    checkbox: BuiltColor
-
-    scrollBar: BuiltColor
-    scrollBarActive: BuiltColor
-
-    statusBar: BuiltColor
-    statusBarForeground: BuiltColor
-    statusBarDebugging: BuiltColor
-    statusBarNoFolder: BuiltColor
-
-    badge: BuiltColor
-    badgeForeground: BuiltColor
-};
+export type BuiltTheme = BaseTheme<BuiltStyle, BuiltColor>;
 
 /** Builds an Oro theme definition. */
 export function buildTheme(theme: OroTheme): BuiltTheme {
